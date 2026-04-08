1de76b96796656ed4a2ee8b8cafe2a32792d6858
Hnato <a.hnatko2077@o2.pl>
2026-04-08 18:42:44 +0200

    v1.5

--- server/obj/Debug/net10.0-windows/win-x64/SquawkServer.AssemblyInfo.cs
+++ server/obj/Debug/net10.0-windows/win-x64/SquawkServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SquawkServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c28802900a665977dac8a98866ea62cbfa2e75f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SquawkServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SquawkServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
